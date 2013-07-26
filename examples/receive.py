6814a71fa9afe4cc7ce9e4f74f5ae99485d7144d
rickhanlonii <rickhanlonii@gmail.com>
2013-07-26 15:41:04 -0400

    Expand example
    This makes it more clear how to add more queues ("channels") to the exchange to receive copies of the message.

--- examples/receive.py
+++ examples/receive.py
@@ -5,7 +5,16 @@ import notifs
 
 
 def print_notification(title, message):
-    print "Notification received: {0}: {1}".format(title, message)
+    print "Print notification received: {0}: {1}".format(title, message)
+
+def web_app_notify(title, message):
+    print "Webapp notification received: {0}: {1}".format(title, message)
+ 
+def iphone_app_notify(title, message):
+    print "iPhone App notification received: {0}: {1}".format(title, message)
+ 
+def android_app_notify(title, message):
+    print "Android App notification received: {0}: {1}".format(title, message)
 
 try:
     n = notifs.Notifs("amqps://user:password@domain.tld:5673/%2F")
@@ -15,6 +24,9 @@ try:
     # This will make it easy to choose which channels your clients
     # will receive. Can be a list too, for listening on multiple streams.
     n.receive("test_channel", print_notification)
+    n.receive("test_channel", web_app_notify)
+    n.receive("test_channel", iphone_app_notify)
+    n.receive("test_channel", android_app_notifify)
 
 except KeyboardInterrupt:
     break
